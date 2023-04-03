643de062754d2582de76bd8b5025b80ab6062d61
matusha's code <mat1073m@gmail.com>
2023-04-03 03:40:24 +0300

    Initial Commit

---
+++ lab3b/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab3b
+//
+//  Created by Матвей Галимьянов on 03.04.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
